3ea2a0ba47b10bd6e72c5ef65241ddaf52fc6217
git_hup <1304740296@qq.com>
2018-10-20 15:43:56 +0800

    新建User.java输出66

--- git_project/src/main/java/User.java
+++ git_project/src/main/java/User.java
@@ -3,4 +3,8 @@ public class User {
     private String name;
     private Integer age;
     private String address;
+
+    public static void main(String[] args) {
+        System.out.println(66);
+    }
 }
